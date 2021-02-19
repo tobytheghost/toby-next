e8c03388ccdc9167b1c373995d68b5cd75e5156e
tobytheghost <tobytheghost@hotmail.com>
2021-02-19 21:00:44 +0000

    Added back to top button

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,46 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import { animateScroll as scroll } from "react-scroll";
+import VisibilitySensor from "react-visibility-sensor";
+import { motion, AnimatePresence } from "framer-motion";
+
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import FacebookIcon from "@material-ui/icons/Facebook";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 
 function Footer() {
+  const scrollToSection = () => {
+    scroll.scrollToTop();
+  };
+
+  const [isVisible, setVisibility] = useState(false);
+
+  const handleOnChange = (visibility) => {
+    setVisibility(visibility);
+  };
+
   return (
-    <section className="section footer">
-      <div className="footer__links">
-        <ul className="footer__items">
-          <li className="footer__item">
-            <a
-              href="https://www.linkedin.com/in/tobygates/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <LinkedInIcon />
-            </a>
-          </li>
-          <li className="footer__item">
-            <a
-              href="https://github.com/tobytheghost/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <GitHubIcon />
-            </a>
-          </li>
-          <li className="footer__item">
-            <a
-              href="https://www.facebook.com/tobytheghost"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FacebookIcon />
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="footer__copy">
-        Website Design & Build - Toby Gates - {new Date().getFullYear()}
-      </div>
-    </section>
+    <VisibilitySensor partialVisibility onChange={handleOnChange}>
+      {() => (
+        <>
+          <section className="section footer">
+            <div className="footer__links">
+              <ul className="footer__items">
+                <li className="footer__item">
+                  <a
+                    href="https://www.linkedin.com/in/tobygates/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <LinkedInIcon />
+                  </a>
+                </li>
+                <li className="footer__item">
+                  <a
+                    href="https://github.com/tobytheghost/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <GitHubIcon />
+                  </a>
+                </li>
+                <li className="footer__item">
+                  <a
+                    href="https://www.facebook.com/tobytheghost"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FacebookIcon />
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="footer__copy">
+              Website Design & Build - Toby Gates - {new Date().getFullYear()}
+            </div>
+          </section>
+          <motion.button
+            className="top-button"
+            onClick={scrollToSection}
+            variants={{
+              show: {
+                opacity: 1,
+              },
+              hide: {
+                opacity: 0,
+              },
+            }}
+            animate={isVisible ? "show" : "hide"}
+            initial={"hide"}
+            exit={"hide"}
+          >
+            <KeyboardArrowUpIcon />
+          </motion.button>
+        </>
+      )}
+    </VisibilitySensor>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,6 @@ const App = () => {
       <About />
       <Technologies />
       <Blog />
-      {/* <Contact /> */}
     </>
   );
 };
